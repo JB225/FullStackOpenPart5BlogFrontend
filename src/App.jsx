99398f082517f20de9a731251462b1732010fdd5
Jacob <jbale444@outlook.com>
2023-09-30 09:57:16 +0100

    Added functionality so a logged in user can create a new blog and so that notification messages shown when users fail to log in or sucessfully create a new blog for exercises 5.3 & 5.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import ErrorNotification from './components/ErrorNotification'
+import SuccessNotification from './components/SuccessNotification'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [successMessage, setSucessMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setURL] = useState('')
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs( blogs ))  
   }, [])
 
   useEffect(() => {
@@ -20,6 +27,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -31,13 +39,13 @@ const App = () => {
         username, password
       })
 
+      blogService.setToken(user.token)
       window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
-      console.log(user)
     } catch (exception) {
-      setErrorMessage('Wrong credentials')
+      setErrorMessage('wrong username or password')
       setTimeout(() => {
         setErrorMessage(null)}, 5000)
     }
@@ -49,10 +57,38 @@ const App = () => {
     setUser(null)
   }
 
+  const handleCreateNewBlog = async (event) => {
+    event.preventDefault()
+
+    try {
+      const newBlog = {
+        title: title,
+        author: author,
+        url: url
+      }
+
+      await blogService.createNewBlog(newBlog)
+      await blogService.getAll().then(blogs => setBlogs( blogs ))
+
+      setTitle('')
+      setAuthor('')
+      setURL('')
+
+      setSucessMessage(`a new blog ${title} by ${author} added`)
+      setTimeout(() => {
+        setSucessMessage(null)}, 5000)
+    } catch (exception) {
+      setErrorMessage('New blog could not be created')
+      setTimeout(() => {
+        setErrorMessaeg(null)}, 5000)
+    }
+  }
+
   if (user === null) {
     return (
       <div>
         <h2>Log in to application</h2>
+        <ErrorNotification message={errorMessage}/>
         <form onSubmit = {handleLogin}>
           <div>
             username <input type="text" value={username} name="Username" onChange={({target}) => setUsername(target.value)}/>
@@ -69,12 +105,24 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <SuccessNotification message={successMessage}/>
+      <ErrorNotification message={errorMessage}/>
       <p>{user.name} is logged in <button onClick={handleLogout}>logout</button></p>
+
+      <h2>create new</h2>
+      <form onSubmit = {handleCreateNewBlog}>
+        <div>title: <input type="text" value={title} name="Title" onChange={({target}) => setTitle(target.value)} /></div>
+        <div>author: <input type="text" value={author} name="Author" onChange={({target}) => setAuthor(target.value)}/></div>
+        <div>url: <input type="text" value={url} name="URL" onChange={({target}) => setURL(target.value)}/></div>
+        <button type="Submit">create</button>
+      </form>
+
+      <br></br>
+      <br></br>
       {blogs.map(blog => <Blog key={blog.id} blog={blog}/>)}
+
     </div>
   )
 }
 
 export default App
-
-// TODO: Add conditional rendering of login form
